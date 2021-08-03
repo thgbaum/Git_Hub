692afd128f3e2f575464c784ce4bccdcf9d4b4af
thgbaum <thg.baum@gmail.com>
2021-08-03 10:02:22 -0300

    3rd

--- for.py
+++ for.py
@@ -1,3 +1,15 @@
 word = 'python'
 for i in word:
     print(i)
+x = 1
+print(x**2)
+x=2
+while True:
+    resultado = x ** 2
+    if resultado > 10000:
+        break
+    print(resultado)
+    x=resultado
+    
+
+   
